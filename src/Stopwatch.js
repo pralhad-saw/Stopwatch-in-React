876a555202b87e73013e48b01b6480324e99e9e9
pralhad-saw <shubhamsav109@gmail.com>
2025-06-16 17:40:59 +0530

    Update Stopwatch.js
    Writing logic behind Stopwatch component with ReactJS.
    Used useState, useCallback, useRef hooks.
    
    And react Stopwatch code is finally done

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,1 +1,60 @@
+import React, {useState, useCallback , useRef } from 'react';
 
+function Stopwatch() {
+const [time, setTime] = useState(0);
+const [isRunning, setIsRunning] = useState(false);
+const intervalRef = useRef(null);
+const start = useCallback(() => {
+if (!isRunning) {
+intervalRef.current = setInterval(() => {
+
+setTime((prevTime) => prevTime + 1);
+
+}, 1000);
+
+setIsRunning (true);
+
+} }, [isRunning]);
+
+const stop = useCallback(() => {
+
+if (isRunning) {
+
+clearInterval(intervalRef.current);
+
+setIsRunning (false);
+
+}
+
+}, [isRunning]);
+
+const reset = useCallback(() => {
+
+clearInterval(intervalRef.current);
+
+setisRunning (false);
+
+setTime(0);
+
+}, []);
+
+return (
+
+<div>
+
+<h1>Stopwatch</h1>
+
+<p>Time: {time} seconds</p>
+
+<button onClick={start}>Start</button>
+
+<button onClick={stop}>Stop</button>
+
+<button onClick={reset)>Reset</button>
+
+</div>
+
+
+  );
+}
+export default Stopwatch;
